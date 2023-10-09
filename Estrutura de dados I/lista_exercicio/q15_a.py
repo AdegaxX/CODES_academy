8a35b5f3e7d66d5902aba6469e8af1bda7ad64c8
Leandro Adegas <ecommerce.adegas@gmail.com>
2023-10-09 19:41:01 -0300

    in

---
+++ Estrutura de dados I/lista_exercicio/q15_a.py
@@ -0,0 +1,15 @@
+
+lista = [1,1,0,0,1,1,1,0,0]
+
+lista.pop()
+print(lista)
+lista.pop()
+print(lista)
+lista.append(0)
+print(lista)
+lista.append(1)
+print(lista)
+lista.append(1)
+print(lista)
+lista.pop()
+print(lista)
