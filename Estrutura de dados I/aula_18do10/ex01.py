965467f0192aa0354f1d5d48615d2a1fb4022aa8
Leandro Adegas <ecommerce.adegas@gmail.com>
2023-10-18 16:32:42 -0300

    in

---
+++ Estrutura de dados I/aula_18do10/ex01.py
@@ -0,0 +1,10 @@
+def cria_grafo(lista_de_vertices, lista_de_arestas):
+    grafo = {}
+    for vertice in lista_de_vertices:
+        grafo[vertice] = []
+    for aresta in lista_de_arestas:
+        grafo[aresta[0]].append(aresta[1])
+    return grafo
+
+lista_de_vertices = ['a','b','c','d','e','f']
+lista_de_aresta = [('a','b'),('b','a'),('b','c'),('c','b'),('c','e'),('e','a'),('e','b')]
